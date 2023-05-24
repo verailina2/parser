6d43359ce07fa999bf0e9748f7bbddaa88b32d60
ezhovkinaju <yulia.ezhovkina@yandex.ru>
2023-05-24 10:43:07 +0300

    Дописала parser

--- parser.py
+++ parser.py
@@ -3,6 +3,16 @@ import requests
 
 url = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
 response = requests.get(url)
-print(response)
-bs = BeautifulSoup(response.text, "lxml")
-print(bs)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+books = soup.find_all('article', class_='product_pod')
+
+for book in books:
+    title = book.h3.a['title']
+
+    price = book.find('p', class_='price_color').text
+
+    print("Название: ", title)
+    print("Цена: ", price)
+    print("---------")
