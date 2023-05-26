838d780d3412f7e2a24487d752ea04f2c029b160
verailina2 <vera.ilina.98@bk.ru>
2023-05-26 13:18:07 +0300

    Исправила ошибку вывода, убрала А перед значком £

--- parser.py
+++ parser.py
@@ -11,8 +11,7 @@ books = soup.find_all('article', class_='product_pod')
 for book in books:
     title = book.h3.a['title']
 
-    price = book.find('p', class_='price_color').text
-
+    price = book.find("p", class_="price_color").text.replace("Â", "")
     print("Название: ", title)
     print("Цена: ", price)
     print("---------")
