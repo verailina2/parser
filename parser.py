c878a3664da1ba5cd9b90c417f028a4f64682c08
lizobakted <lizobakt.ed@yandex.ru>
2023-05-26 13:57:40 +0300

    Отформатировала код

--- parser.py
+++ parser.py
@@ -1,17 +1,16 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
+
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
 books = soup.find_all('article', class_='product_pod')
 
 for book in books:
     title = book.h3.a['title']
-
     price = book.find("p", class_="price_color").text.replace("Â", "")
+
     print("Название: ", title)
     print("Цена: ", price)
-    print("---------")
+    print()
